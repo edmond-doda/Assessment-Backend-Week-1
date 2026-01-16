2d1d4da1f8a5e3e108968b62d31fe12ef1d47b0c
edmond-doda <edmonddodaa@gmail.com>
2026-01-16 15:02:54 +0000

    date_functions working

--- days_api/date_functions.py
+++ days_api/date_functions.py
@@ -1,19 +1,29 @@
 """Functions for working with dates."""
 
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 
 
 def convert_to_datetime(date_val: str) -> datetime:
-    pass
+    date_object = datetime.strptime(date_val, "%d.%m.%Y")
+    return date_object
 
 
 def get_days_between(first: datetime, last: datetime) -> int:
-    pass
+    delta = last - first
+    return delta.days
 
 
 def get_day_of_week_on(date_val: datetime) -> str:
-    pass
+    day = datetime.strftime(date_val, '%A')
+    return day
 
 
 def get_current_age(birthdate: date) -> int:
-    pass
+    today = date.today()
+    age = today.year - birthdate.year
+    if today.month < birthdate.month:
+        age -= 1
+    if ((today.month == birthdate.month) and
+            today.day < birthdate.day):
+        age -= 1
+    return age
