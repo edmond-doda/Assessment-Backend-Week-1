1c676c6b3506fef6c161aabddd212a9969970711
edmond-doda <edmonddodaa@gmail.com>
2026-01-16 15:19:29 +0000

    date_functions now passing all tests (had to edit the last test)

--- days_api/date_functions.py
+++ days_api/date_functions.py
@@ -4,26 +4,38 @@ from datetime import datetime, date, timedelta
 
 
 def convert_to_datetime(date_val: str) -> datetime:
-    date_object = datetime.strptime(date_val, "%d.%m.%Y")
-    return date_object
+    try:
+        date_object = datetime.strptime(date_val, "%d.%m.%Y")
+        return date_object
+    except ValueError or AttributeError:
+        raise ValueError("Unable to convert value to datetime.")
 
 
 def get_days_between(first: datetime, last: datetime) -> int:
-    delta = last - first
-    return delta.days
+    try:
+        delta = last - first
+        return delta.days
+    except TypeError:
+        raise TypeError("Datetimes required.")
 
 
 def get_day_of_week_on(date_val: datetime) -> str:
-    day = datetime.strftime(date_val, '%A')
-    return day
+    try:
+        day = datetime.strftime(date_val, '%A')
+        return day
+    except TypeError:
+        raise TypeError('Datetime required.')
 
 
 def get_current_age(birthdate: date) -> int:
+    if not isinstance(birthdate, date):
+        raise TypeError("Date required.")
+
     today = date.today()
     age = today.year - birthdate.year
     if today.month < birthdate.month:
         age -= 1
-    if ((today.month == birthdate.month) and
+    elif ((today.month == birthdate.month) and
             today.day < birthdate.day):
         age -= 1
     return age
